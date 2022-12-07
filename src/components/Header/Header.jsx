3f2c1945ba03a9141edc7ad9ecbfba4e4e175dfd
Rutwik187 <rutwikshinde34@gmail.com>
2022-12-07 23:46:49 +0530

    initialized the project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,74 @@
+import React, { useRef } from "react";
+import { Container } from "reactstrap";
+import "./header.css";
+
+const navLinks = [
+  {
+    display: "Home",
+    url: "#",
+  },
+  {
+    display: "About",
+    url: "#",
+  },
+
+  {
+    display: "Courses",
+    url: "#",
+  },
+  {
+    display: "Pages",
+    url: "#",
+  },
+  {
+    display: "Blog",
+    url: "#",
+  },
+];
+
+const Header = () => {
+  const menuRef = useRef();
+
+  const menuToggle = () => menuRef.current.classList.toggle("active__menu");
+
+  return (
+    <header className="header">
+      <Container>
+        <div className="navigation d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <h2 className=" d-flex align-items-center gap-1">
+              <img
+                className="logo-jspm"
+                src="https://alumni.herokuapp.com/static/media/LogoRSCOE.605c13fc33b8f7d46fa1.png"
+              />
+            </h2>
+          </div>
+
+          <div className="nav d-flex align-items-center gap-5">
+            <div className="nav__menu" ref={menuRef} onClick={menuToggle}>
+              <ul className="nav__list">
+                {navLinks.map((item, index) => (
+                  <li key={index} className="nav__item">
+                    <a href={item.url}>{item.display}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* <div className="nav__right">
+              <p className="mb-0 d-flex align-items-center gap-2">Login</p>
+            </div> */}
+          </div>
+
+          <div className="mobile__menu">
+            <span>
+              <i class="ri-menu-line" onClick={menuToggle}></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
